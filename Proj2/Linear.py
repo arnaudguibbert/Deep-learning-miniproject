2d705e0d30ebbed8dd606d16300979226989dd3a
Arnaud Guibbert <[arnaud.guibbert@epfl.ch]>
2021-04-09 16:28:06 +0200

    small modifications on Linear module

--- Proj2/Linear.py
+++ Proj2/Linear.py
@@ -1,14 +1,23 @@
-from torch import torch.empty
-torch.set_grad_enabled(False)
+from torch import empty
 
 class Linear():
     
     def __init__(self,in_size,out_size,add_bias=True):
-        self.weights = torch.empty(out_size,in_size).normal_(mean=0,std=2)
-        self.grdweights = torch.zeros_like(self.weights)
-        if add_bias:
-            self.bias = torch.empty(out_size,1).normal_(mean=0,std=2)
-            self.grdbias = torch.zeros_like(self.bias)
+        """
+        Goal:
+        Inputs:
+        in_size = int - Input size of the layer
+        out_size = int - Output size of the layer
+        add_bias = bool - Specify if you want to add a bias or not
+        Outputs:
+        """
+        # Initialize the weights and the associated gradient
+        self.weights = empty(out_size,in_size).normal_(mean=0,std=2) # To be changed
+        self.grdweights = empty(out_size,in_size).fill_(0)
+        if add_bias: 
+            # Initialize the bias
+            self.bias = empty(out_size,1).normal_(mean=0,std=2)
+            self.grdbias = empty(out_size,1).fill_(0)
         else:
             self.bias = None
             self.grdbias = None
@@ -33,9 +42,9 @@ class Linear():
         return grdwrtinput
     
     def zero_grad(self):
-        grdweights = torch.zeros_like(self.weights)
+        self.grdweights = empty(self.weights.shape).fill_(0)
         if self.bias is not None:           
-            grdbias = torch.zeros_like(self.bias)
+            self.grdbias = empty(self.bias.shape).fill_(0)
         
     def optimization_step(self,lr):
         if not self.back:
