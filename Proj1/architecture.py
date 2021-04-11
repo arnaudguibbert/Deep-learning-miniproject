393895a1b07b0bb8f45331d87bbc131a918026a4
robinhood75 <72042118+robinhood75@users.noreply.github.com>
2021-04-11 19:30:46 +0200

    created naive net and trained it

--- Proj1/architecture.py
+++ Proj1/architecture.py
@@ -1,2 +1,18 @@
 import torch
 import torch.nn as nn
+
+def create_naive_net():
+    model = nn.Sequential(
+        nn.Conv2d(2, 16, kernel_size=3),
+        nn.MaxPool2d(2),
+        nn.ReLU(),
+        nn.BatchNorm2d(16),
+        nn.Conv2d(16, 32, 3),
+        nn.MaxPool2d(2),
+        nn.ReLU(),
+        nn.BatchNorm2d(32),
+        nn.Flatten(),
+        nn.Linear(128, 2),
+        nn.Sigmoid()
+    )
+    return model
--- Proj1/metrics.py
+++ Proj1/metrics.py
@@ -1,1 +1,28 @@
 import torch
+import torch.nn as nn
+
+def train_model(model, train_input, train_target, nb_epochs, mini_batch_size = 100, eta = 0.1, criterion = nn.CrossEntropyLoss()):
+    
+    optimizer = torch.optim.SGD(model.parameters(), lr = eta)
+    
+    for e in range(nb_epochs):
+        for b in range(0, train_input.size(0), mini_batch_size):
+            output = model(train_input.narrow(0, b, mini_batch_size))
+            loss = criterion(output, train_target.narrow(0, b, mini_batch_size))
+            model.zero_grad()
+            loss.backward()
+            optimizer.step()
+     
+            
+def compute_nb_errors(model, data_input, data_target, mini_batch_size=100):
+
+    nb_data_errors = 0
+
+    for b in range(0, data_input.size(0), mini_batch_size):
+        output = model(data_input.narrow(0, b, mini_batch_size))
+        _, predicted_classes = torch.max(output, 1)
+        for k in range(mini_batch_size):
+            if data_target[b + k] != predicted_classes[k]:
+                nb_data_errors = nb_data_errors + 1
+
+    return nb_data_errors
