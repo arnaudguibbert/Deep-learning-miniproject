5f44619b446b77b3694d55fda4261100fd25de4f
robinhood75 <72042118+robinhood75@users.noreply.github.com>
2021-04-11 20:07:23 +0200

    created naive net and trained it, added useful methods to metrics.py

--- Proj1/metrics.py
+++ Proj1/metrics.py
@@ -1,5 +1,7 @@
 import torch
 import torch.nn as nn
+import matplotlib.pyplot as plt
+import numpy as np
 
 def train_model(model, train_input, train_target, nb_epochs, mini_batch_size = 100, eta = 0.1, criterion = nn.CrossEntropyLoss()):
     
@@ -26,3 +28,24 @@ def compute_nb_errors(model, data_input, data_target, mini_batch_size=100):
                 nb_data_errors = nb_data_errors + 1
 
     return nb_data_errors
+
+
+
+def plot_error_vs_epochs(model, max_nb_epochs, step, train_input, train_target, test_input, test_target):
+    """
+    To be checked.
+    Goal: plot number of errors in function of nb epochs on the stream ie while training the model (for efficiency reasons)
+    """
+    error_rates = []
+    
+    for nb_epochs in range(0, max_nb_epochs//step):
+        # Train model for 'step' epochs, compute error at each iteration
+        train_model(model, train_input, train_target, nb_epochs=step)
+        error = compute_nb_errors(model, test_input, test_target)/test_input.size(0)
+        error_rates.append(error)
+        
+    plt.plot(np.arange(0, max_nb_epochs, step), error_rates)
+    plt.xlabel("number of epochs")
+    plt.ylabel("error rate")
+    plt.title("Naive Net")
+    plt.show()
