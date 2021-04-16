83deebc4f9223871a9d743ec1a6cabdfca83c92c
robinhood75 <72042118+robinhood75@users.noreply.github.com>
2021-04-16 15:04:40 +0200

    solve all problems

--- Proj1/architecture.py
+++ Proj1/architecture.py
@@ -1,6 +1,22 @@
 import torch
 import torch.nn as nn
 
+<<<<<<< HEAD
+def create_naive_net():
+    model = nn.Sequential(
+        nn.Conv2d(2, 16, kernel_size=3),
+        nn.MaxPool2d(2),
+        nn.ReLU(),
+        nn.BatchNorm2d(16),
+        nn.Conv2d(16, 32, 3),
+        nn.MaxPool2d(2),
+        nn.ReLU(),
+        nn.BatchNorm2d(32),
+        nn.Flatten(),
+        nn.Linear(128, 2)
+    )
+    return model
+=======
 class Naive_net(nn.Module):
 
     def __init__(self):
@@ -47,3 +63,4 @@ class Naive_net_2(nn.Module):
     def forward(self,input):
         output = self.sequence(input)
         return output
+>>>>>>> 9d98d90f25ae7e18348867cce8d9f8cbba7422e8
